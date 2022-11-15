622e71ee8982f288d5399622ebf7e3d0b4ac5529
Mateusz <mateusz.urbanczyk11@gmail.com>
2022-11-15 23:24:15 +0100

    Interface of logging page done

---
+++ interfejs/logging.js
@@ -0,0 +1,49 @@
+function login() {
+  //to_delete
+  var myName = arguments.callee.toString();
+  myName = myName.substr("function ".length);
+  myName = myName.substr(0, myName.indexOf("("));
+
+  alert("Called function:" + myName);
+  //to_delete
+}
+
+function password_forgotten() {
+  //to_delete
+  var myName = arguments.callee.toString();
+  myName = myName.substr("function ".length);
+  myName = myName.substr(0, myName.indexOf("("));
+
+  alert("Called function:" + myName);
+  //to_delete
+}
+
+function no_account() {
+  //to_delete
+  var myName = arguments.callee.toString();
+  myName = myName.substr("function ".length);
+  myName = myName.substr(0, myName.indexOf("("));
+
+  alert("Called function:" + myName);
+  //to_delete
+}
+
+function go_to_main_page() {
+  //to_delete
+  var myName = arguments.callee.toString();
+  myName = myName.substr("function ".length);
+  myName = myName.substr(0, myName.indexOf("("));
+
+  alert("Called function:" + myName);
+  //to_delete
+}
+
+function show_basket() {
+  //to_delete
+  var myName = arguments.callee.toString();
+  myName = myName.substr("function ".length);
+  myName = myName.substr(0, myName.indexOf("("));
+
+  alert("Called function:" + myName);
+  //to_delete
+}
