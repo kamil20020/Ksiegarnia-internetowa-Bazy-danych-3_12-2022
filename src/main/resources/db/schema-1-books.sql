b03076c4ce2aeefdf4c20932aa38e452ac3f04c9
kdywan_254049 <idywan_254049@e-science.pl>
2022-11-19 23:42:55 +0100

    Added mysql connector and added dev and prod profiles, some maven dependencies

---
+++ src/main/resources/db/schema-1-books.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Books (
+    id int(9) NOT NULL AUTO_INCREMENT,
+    title varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+);
--- src/main/resources/db/schema-books.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE Books (id int(9) NOT NULL AUTO_INCREMENT, title varchar(100) NOT NULL, PRIMARY KEY (id));
