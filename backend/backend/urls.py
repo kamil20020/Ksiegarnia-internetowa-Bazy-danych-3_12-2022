58ded443a24f7c86d5eabb25c5667babb4b14307
Mateusz <252808@student.pwr.edu.pl>
2022-12-01 21:22:58 +0100

    Problem with refreshing resolved. Maybe bad solution, but better whatever than nothing

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
   path('', views.return_page),
   path('books/all', views.get_start_page_data),
   path('books/find/', views.find_books),
-  path('book/<int:id>', views.get_book)
+  path('book/<int:id>', views.get_book),
+  path('<string>', views.return_page)
 ]
