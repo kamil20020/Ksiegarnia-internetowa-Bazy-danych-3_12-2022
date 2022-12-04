50bbec0fa3a90388f7b5661869848bf52ce998cf
Mateusz <252808@student.pwr.edu.pl>
2022-12-04 15:27:28 +0100

    Clear branch

--- backend/database/class_attributes.py
+++
@@ -1,9 +0,0 @@
-
-
-class ClassAttributes:
-    @staticmethod
-    def get_class_attributes(cl):
-        fields = cl.__doc__
-        fields = fields[6:]
-        fields = fields[:-1]
-        return list(fields.split(", "))
